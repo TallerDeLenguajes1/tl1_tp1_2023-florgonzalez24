b17a57180ca4c9fe98865845852c901d2fdcdf78
Florencia <iturbe.mariaflorencia@gmail.com>
2023-04-28 18:00:08 -0300

    agregue punto 1

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+
+    int num = 1;
+    int *punt = &num;
+
+    printf("contenido del puntero: %d\n", punt);
+    printf("Direccion de memoria almacenada por el puntero: %d\n", punt);
+    printf("Direccion de memoria de la variable: %d\n", &num);
+    printf("Direccion de memoria del puntero: %d\n", &punt);
+    int tama = sizeof(punt);
+    printf("el tamanio de memoria utilizada por la variable: %d\n", tama);
+
+
+
     return 0;
 }
